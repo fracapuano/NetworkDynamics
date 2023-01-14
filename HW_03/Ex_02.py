a98a50bc9d7f470f6af70ee557b06d44a238e67c
fracapuano <francesco_capuano@aol.com>
2023-01-14 19:05:17 +0100

    fix: debug and add prints

--- HW_03/Ex_02.py
+++ HW_03/Ex_02.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from tqdm import tqdm
 import statistics
 from HW_03.coloring.SimpleLineProblem import SimpleLine
+import random
 from HW_03.coloring.WifiProblem import WifiAssignment
 
 
@@ -32,16 +33,17 @@ def execute():
 
         colour_dict = simple_line.nodes_statistics
         potentials.append(simple_line.find_potential())
+    
+    print("Final Potential: {:.4f}".format(potentials[-1]))
 
     # Exercise 2.2
-    import random
     print('Exercise 2.2')
 
     wifi_problem = WifiAssignment()
     wifi_problem2 = WifiAssignment()
     # initialise the state of the system at t = 0, according to what prescribed in the text
 
-    colorlist_all = random.choices(["red"], k=100)
+    colorlist_all = ["red"] * 100
     colorlist = random.choices(["red", "green", "blue", "yellow", "magenta", "cyan", "white", "black"], k=100)
     # We initialized two wifi assignment instances in order to study the evolution of the dynamics from two different starting configurations
     wifi_problem.init_nodes(colorlist)
@@ -51,7 +53,7 @@ def execute():
     potentials2 = []
     node_states_history = []
 
-    for t in tqdm(range(5_000), desc="Simulating"):
+    for t in tqdm(range(5000), desc="Simulating (from all red)"):
         # choose uniformly at random one node
         node = np.random.choice(wifi_problem.nodes)
         # for each colour, retrieve the probability that this node will be of that colour at the next timestamp
@@ -63,10 +65,11 @@ def execute():
 
         potentials.append(wifi_problem.find_potential())
         node_states_history.append(wifi_problem.nodes)   
-        
+    print("Final potential (from all red): {:.4f}".format(potentials[-1]))
+
     potentials2 = []
     node_states_history = []
-    for t in tqdm(range(5_000), desc="Simulating"):
+    for t in tqdm(range(5000), desc="Simulating (from random config)"):
         # choose uniformly at random one node
         node = np.random.choice(wifi_problem2.nodes)
         # for each colour, retrieve the probability that this node will be of that colour at the next timestamp
@@ -78,17 +81,25 @@ def execute():
 
         potentials2.append(wifi_problem2.find_potential())
         node_states_history.append(wifi_problem2.nodes)
+    print("Final potential (from random config): {:.4f}".format(potentials[-1]))
 
     # Exercise 2.3
     print('Exercise 2.3')
     mean_potentials = []
-
     all_potentials = []
+    print("Simulating with various etachoices...")
+
+    expressions = {
+        1: "eta(t) = 0, for all t >= 0",
+        2: "eta(t) = 3, for all t >= 0",
+        3: "eta(t) = 100, for all t >= 0",
+        4: "eta(t) = t^4/100, for all t >= 0"
+    }
 
     for etachoice in [1,2,3,4]: # Check the report for etachoice functional expression
         wifi_problem = WifiAssignment()
         # initialise the state of the system at t = 0, according to what prescribed in the text
-        wifi_problem.init_nodes('red')
+        wifi_problem.init_nodes(['red'] * 100)
         potentials = []
         node_states_history = []
         for t in tqdm(range(5_000), desc="Simulating"):
@@ -107,7 +118,9 @@ def execute():
 
             potentials.append(wifi_problem.find_potential())
             node_states_history.append(wifi_problem.nodes)
+
+        print(f"With etachoice: {expressions[etachoice]}:")
+        print(f"\tAverage Potential: {statistics.mean(potentials)}")
             
         all_potentials.append(potentials)
-        
         mean_potentials.append(statistics.mean(potentials))
